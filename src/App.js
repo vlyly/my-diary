ecc1a106fc29bbfc7cec3cee12ab06698ef19fc9
S2oonae <S2oonae@gmail.com>
2021-12-14 17:54:45 +0900

    🔨 Implement structure of App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { useEffect, useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Signin from './routes/sigin'
-
+import Main from './routes/main'
 function App({ authService }) {
+  const [init, setInit] = useState(false)
+  const [user, setUser] = useState({ isLogin: false })
+
+  useEffect(() => {
+    authService.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user)
+        setUser({ isLogin: true })
+      } else {
+        console.log('...')
+        setUser({ isLogin: false })
+      }
+      setInit(true)
+    })
+  }, [user.isLogin, authService]) //user나 prop으로 받아온 authService가 변경되었을 때 useEffect를 실행합니다.
+
   return (
     <div className="App">
-      <Signin authService={authService} />
+      {init ? (
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                user.isLogin ? (
+                  <Main authService={authService} user={user} />
+                ) : (
+                  <Signin authService={authService} user={user} />
+                )
+              }
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        'is initializing...'
+      )}
     </div>
   )
 }
